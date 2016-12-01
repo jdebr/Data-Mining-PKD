291ca4f39c8dae555b4919d733f94d62de67007c
jdebr <jd.debr@gmail.com>
2016-11-30 21:13:30 -0700

    Updated knni and added first properly imputed data file

--- code/knni.py
+++ code/knni.py
@@ -7,7 +7,9 @@ import csv
 dataPath = '../data/cleaned data/'
 writePath = '../data/imputed data/'
 
-for fn in os.listdir(dataPath):
+#for fn in os.listdir(dataPath):
+for i in range(1):
+  fn = '06037500-cleaned'
 
   dataFile = dataPath + fn
   newFile = writePath + fn + "-imputed"
@@ -21,27 +23,26 @@ for fn in os.listdir(dataPath):
   with open(dataFile,'r') as f:
     rawData = f.read().splitlines()
     for ind, line in enumerate(rawData):
-      if ind > 0:
-        dat = line.split(',')
-        originalData.append(dat)
-        skip = False
-        # currently 6 features
-        for i in range(1,7):
-          if dat[i].strip() == 'M' or dat[i].strip() == '':
-            #print("skipped")
-            skip = True
-        # if there are no missing values, add to dataset  
-        if not skip:
-          # add features: 1-watertemp, 2-discharge, 3-conductance, 4-gage height, 5-airTemp, 6-hourly precip
-          newDat = [float(dat[2]), float(dat[5])]
-          dataSet.append(newDat)
-          # imputing watertemp
-          labels.append(float(dat[1]))
-        # if water temp is missing but discharge and airtemp are there, add to imputationSet 
-        elif dat[1].strip() == '' and not dat[2].strip() == '' and not dat[5].strip() == 'M':
-          newDat = [float(dat[2]), float(dat[5])]
-          imputationSet.append(newDat)
-          impIndex.append(ind)
+      dat = line.split(',')
+      originalData.append(dat)
+      skip = False
+      # currently 6 features
+      for i in range(1,7):
+        if dat[i].strip() == 'M' or dat[i].strip() == '':
+          #print("skipped")
+          skip = True
+      # if there are no missing values, add to dataset  
+      if not skip:
+        # add features: 1-watertemp, 2-discharge, 3-conductance, 4-gage height, 5-airTemp, 6-hourly precip
+        newDat = [float(dat[2]), float(dat[5])]
+        dataSet.append(newDat)
+        # imputing watertemp
+        labels.append(float(dat[1]))
+      # if water temp is missing but discharge and airtemp are there, add to imputationSet 
+      elif dat[1].strip() == '' and not dat[2].strip() == '' and not dat[5].strip() == 'M':
+        newDat = [float(dat[2]), float(dat[5])]
+        imputationSet.append(newDat)
+        impIndex.append(ind)
         
   #print(len(dataSet))
   #print(len(labels))
@@ -58,15 +59,14 @@ for fn in os.listdir(dataPath):
   # Add imputed values to original data 
   valCount = 0
   for i in impIndex:
-    originalData[i][1] = imputedValues[valCount]
+    originalData[i][1] = round(imputedValues[valCount], 1)
     valCount += 1
     
   
   # Write all data to new file 
   with open(newFile, 'w') as w:
-    wr = csv.writer(w, quoting=csv.QUOTE_ALL)
-    for line in originalData:
-      wr.writerow(line)
+    writer = csv.writer(w, lineterminator='\n')
+    writer.writerows(originalData)
 
   ''' 
   # This section runs an experiment demonstrating efficacy of imputation 
